0b3bed2d1d51927c417c805b07b7118c0c7dcb07
Serge Meeuwsen <serge.meeuwsen@gmail.com>
2024-04-25 12:21:21 +0100

    Updated script with pre and post license application information

--- apply-vcenter-license.ps1
+++ apply-vcenter-license.ps1
@@ -1,12 +1,12 @@
 <#
 
-  Simple script to apply vCenter licenses
+  Simple script to add and apply vCenter licenses on vCenter 6.5
 
 #>
 # Set vCenter credentials
 $vCenterServer = "FQDN of your vCenter"
-$License = "License Key"
-
+$licKey = '11111-22222-33333-44444-55555'
+$licType = 'vc.essentials.instance'
 
 $username = "administrator@vsphere.local"
 $password = ConvertTo-SecureString "mypassword" -AsPlainText -Force
@@ -15,11 +15,23 @@ $psCred = New-Object System.Management.Automation.PSCredential -ArgumentList ($u
 
 # Connect to the vCenter
 Disconnect-VIServer -confirm:$false -ErrorAction SilentlyContinue
-Write-Host "Now connecting to $vCenterServer"
+Write-Host "Now connecting to $vCenterServer" -ForegroundColor Yellow
 $vCenter = Connect-VIServer -Server $vCenterServer -Credential $psCred
 
-# Assign/update license key for this particular vCenter:
-$LM = get-view($vCenter.ExtensionData.content.LicenseManager)
-$LM.AddLicense($License,$null)
-$LAM = get-view($LicenseManager.licenseAssignmentManager)
-$LAM.UpdateAssignedLicense($vCenter.InstanceUuid,$License,$Null)
+
+# Show licensing info before adding and assigning license
+Write-Host "Current Licensing Information on $vCenterServer" -ForegroundColor Yellow
+$licMgr = Get-View licensemanager
+$licMgr.Licenses
+
+
+# Add and Assign license key for this particular vCenter:
+Write-Host "Adding and assigning Licensing license key $licKey on $vCenterServer" -ForegroundColor Yellow
+$licMgr.AddLicense($licKey, $null)
+$lic = $licMgr.Licenses.Where{ $_.EditionKey -eq $licType }
+$licAssMgr = Get-View $licMgr.LicenseAssignmentManager
+$licAssMgr.UpdateAssignedLicense($global:DefaultVIServer.InstanceUuid, $lic.LicenseKey, $global:defaultVIServer.Name)
+
+# Show licensing info after adding and assigning the license
+Write-Host "Updated Licensing Information on $vCenterServer" -ForegroundColor Yellow
+$licMgr.Licenses
